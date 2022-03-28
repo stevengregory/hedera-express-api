6f73a3d6736922e5a63b1c658f21775669a31ec0
stevengregory <sgregory@gmail.com>
2022-03-28 18:27:36 -0500

    refactor: make one instance of client

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -3,13 +3,13 @@ import { AccountBalanceQuery, AccountCreateTransaction, Hbar, PrivateKey, Transf
 import HederaService from '@services/hedera.service';
 
 class AccountController {
-  public hederaService = new HederaService();
+  private hederaService = new HederaService();
+  private client = this.hederaService.getClient();
 
   public getAccountBalance = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const client = this.hederaService.getClient();
       const accountId = req.params.accountId;
-      const balance = await new AccountBalanceQuery().setAccountId(accountId).execute(client);
+      const balance = await new AccountBalanceQuery().setAccountId(accountId).execute(this.client);
       console.log(`${accountId.toString()} balance = ${balance.hbars.toString()}`);
       res.status(200).json({
         data: { accountId: accountId.toString(), balance: balance.hbars.toString() },
@@ -22,12 +22,11 @@ class AccountController {
 
   public createAccount = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const client = this.hederaService.getClient();
       const newKey = PrivateKey.generate();
       console.log(`private key = ${newKey.toString()}`);
       console.log(`public key = ${newKey.publicKey.toString()}`);
-      const response = await new AccountCreateTransaction().setInitialBalance(new Hbar(10)).setKey(newKey.publicKey).execute(client);
-      const receipt = await response.getReceipt(client);
+      const response = await new AccountCreateTransaction().setInitialBalance(new Hbar(10)).setKey(newKey.publicKey).execute(this.client);
+      const receipt = await response.getReceipt(this.client);
       console.log(`account id = ${receipt.accountId.toString()}`);
       res.status(200).json({
         data: { accountId: receipt.accountId.toString(), publicKey: newKey.publicKey.toString(), privateKey: newKey.toString() },
@@ -40,7 +39,6 @@ class AccountController {
 
   public transferHbar = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const client = this.hederaService.getClient();
       const accountId = req.params.accountId;
       const transferAmount = 10;
       const newAccountPrivateKey = await PrivateKey.generateED25519();
@@ -48,21 +46,21 @@ class AccountController {
       const newAccountTransactionResponse = await new AccountCreateTransaction()
         .setKey(newAccountPublicKey)
         .setInitialBalance(Hbar.fromTinybars(1000))
-        .execute(client);
-      const getReceipt = await newAccountTransactionResponse.getReceipt(client);
+        .execute(this.client);
+      const getReceipt = await newAccountTransactionResponse.getReceipt(this.client);
       const newAccountId = getReceipt.accountId;
       console.log(`The new account ID is: ${newAccountId}`);
-      const accountBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+      const accountBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(this.client);
       console.log(`The new account balance is: ${accountBalance.hbars.toTinybars()} tinybar.`);
       const sendHbar = await new TransferTransaction()
         .addHbarTransfer(accountId, Hbar.fromTinybars(-transferAmount))
         .addHbarTransfer(newAccountId, Hbar.fromTinybars(transferAmount))
-        .execute(client);
-      const transactionReceipt = await sendHbar.getReceipt(client);
+        .execute(this.client);
+      const transactionReceipt = await sendHbar.getReceipt(this.client);
       console.log(`The transfer transaction from my account to the new account was: ${transactionReceipt.status.toString()}`);
-      const queryCost = await new AccountBalanceQuery().setAccountId(newAccountId).getCost(client);
+      const queryCost = await new AccountBalanceQuery().setAccountId(newAccountId).getCost(this.client);
       console.log(`The cost of query is: ${queryCost}`);
-      const getNewBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+      const getNewBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(this.client);
       console.log(`The account balance after the transfer is: ${getNewBalance.hbars.toTinybars()} tinybar.`);
       res.status(200).json({
         data: {
