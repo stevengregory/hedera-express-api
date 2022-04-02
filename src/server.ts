5602de8cf262113117bd14850fe5eb7d7b09954a
stevengregory <sgregory@gmail.com>
2022-04-02 13:30:29 -0500

    chore: remove sample code & dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import App from '@/app';
-import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
-import UsersRoute from '@routes/users.route';
 import AccountRoute from '@/routes/account.route';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new AccountRoute()]);
+const app = new App([new IndexRoute(), new AccountRoute()]);
 
 app.listen();
